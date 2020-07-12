227372b70dcd92f84ce7f0ebdba7d8451536cda0
Franco Sanchez <francorsr98@gmail.com>
2020-07-11 23:17:50 -0500

    Desarrollo con componentes y rutas

--- reto-2.1/src/App.vue
+++ reto-2.1/src/App.vue
@@ -5,7 +5,6 @@
 </template>
 
 <style lang="scss">
-
 * {
   margin: 0;
   padding: 0;
@@ -15,28 +14,4 @@
 body {
   font-family: Roboto;
 }
-
-h1 {
-  font-size: 2.2rem;
-}
-
-h2 {
-  font-size: 1.8rem;
-  font-weight: bold;
-}
-
-h3{
-  font-size: .9rem;
-  font-weight: bold;
-}
-
-section {
-  margin-top: 3%;
-  margin-bottom: 3%;
-}
-
-a {
-  text-decoration: none;
-}
-
 </style>
